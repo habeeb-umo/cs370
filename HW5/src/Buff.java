a80d36d4b24899741b88c309e88e41908b3c1df7
Habeeb Mohammed <habeeb.umo@gmail.com>
2017-05-11 19:54:33 -0600

    added HW5

--- HW5/src/Buff.java
+++ HW5/src/Buff.java
@@ -47,5 +47,22 @@ public class Buff extends Thread {
 			return -1;
 		}
 	}
+
+}
+
+class ProducerConsumer{
+	public static void main(String[] args){
+		Buff buff = new Buff();
+		Producer p = new Producer(buff);
+		Consumer c = new Consumer(buff);
+		p.start();
+		c.start();
+		try{
+			p.join();
+			c.interrupt();
+		} catch (InterruptedException e){
+		}
+		System.out.println("Exiting!");
+	}
 	
 }
